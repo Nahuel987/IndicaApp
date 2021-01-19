84d9d386a8a21892820e06c85283f3941c08a39f
Nahuel987 <nahuelcheojavier@gmail.com>
2021-01-19 13:11:26 -0300

    02 Commit Se agregan primeras vistas

--- app/src/main/java/com/example/indicaapp/vista/MainActivity.kt
+++ app/src/main/java/com/example/indicaapp/vista/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.indicaapp
+package com.example.indicaapp.vista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.indicaapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.contenedorFragmentos,FragmentoMenu.newInstance("FG","DESDE ACTIVITY")).commit()
+
     }
 }
